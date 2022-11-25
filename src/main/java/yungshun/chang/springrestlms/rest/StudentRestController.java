bebdc84add5bf920af87d1fb833c3d9549242d15
yungshun317 <seraph317@gmail.com>
2022-11-25 19:19:23 +0800

    [FEAT] Define the endpoint for `/students/{studentId}` and include the `javax.annotation-api` dependency for using `@PostConstruct`

--- src/main/java/yungshun/chang/springrestlms/rest/StudentRestController.java
+++ src/main/java/yungshun/chang/springrestlms/rest/StudentRestController.java
@@ -1,10 +1,12 @@
 package yungshun.chang.springrestlms.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yungshun.chang.springrestlms.entity.Student;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,15 +14,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+
+        students = new ArrayList<>();
 
         students.add(new Student("Kula", "Diamond"));
         students.add(new Student("Morrigan", "Aensland"));
         students.add(new Student("Mai", "Shiranui"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return students;
+    }
+    
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        return students.get(studentId);
     }
 }
